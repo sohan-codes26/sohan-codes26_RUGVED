73e6218b3ad335996ba6e1d2602cbe0cc4371c2d
sravanthi <sravanthi@partsmojotech.com>
2026-09-11 17:00:03 +0530

    Add solution for Task 4

---
+++ Task4.py
@@ -0,0 +1,11 @@
+def selection_sort(s):
+    a = list(s)
+    for i in range(len(a)):
+        m = i
+        for j in range(i + 1, len(a)):
+            if a[j] < a[m]:
+                m = j
+        a[i], a[m] = a[m], a[i]
+    return ''.join(a)
+s = input("Enter string: ")
+print(selection_sort(s))
